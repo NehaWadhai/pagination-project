1373fd0ad6aa8cded89d72ec461a721b3fc2d593
nehawadhai <62004298+NehaWadhai@users.noreply.github.com>
2026-04-03 08:15:15 +0000

    Pagination products loading with image

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+
 const Pagination = () => {
-  return <div>Hello</div>;
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    const response = await fetch(
+      "https://dummyjson.com/products?_gl=1*1m7ilfo*_ga*NTM5NzI4NzE4LjE3NzM0NDQ1MzE.*_ga_TPY1RGQLFR*czE3NzUyMDMyNDUkbzMkZzAkdDE3NzUyMDMyNDkkajU2JGwwJGgw"
+    );
+
+    const data = await response.json();
+    setProducts(data.products);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      {products.map((ele) => {
+        return (
+          <div>
+            <img src={ele.images[0]} />;
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Pagination;
